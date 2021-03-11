587e1a41078eb5e91241b6bc895bd6fc854f9635
Matthew Vanni <mvanni4994@gmail.com>
2021-03-10 20:46:07 -0500

    skeleton of homework

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+//https://www.npmjs.com/package/morgan - Morgan Information
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+// Server connects to 1. Local Mongo Database, OR 2. Atlas Link
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
