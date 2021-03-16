d0744125d527608c8878bc73a19467080eb41aa3
Matthew Vanni <mvanni4994@gmail.com>
2021-03-16 02:36:20 -0400

    finally fixed pathing

--- routes/api.js
+++ routes/api.js
@@ -1,65 +1,38 @@
-const router = require("express").Router();
-const workout = require("../models/workout");
-const express = require("express")
-const app = express();
-const db = require("../models/workout")
+const db = require("../models/workout");
+const Workout = require('../models/workout')
 
-// https://mongoosejs.com/docs/models.html - Mongoose Information
-module.exports = function(app) {
-
-  app.get("/api/workout", function(req, res) {
-    db.workout.find({})
-    .then(function(workout) {
-      res.json(workout);
-    })
-    .catch(err =>{
-res.json(err);
+module.exports = (app) => {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}, (err, workouts) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(workouts);
+      }
     });
   });
-}
 
-app.post("/api/workout", async (req, res)=> {
-  try {
-    const response = await db.Workout.create({type: "workout"})
-    res.json(response);
-  } 
-  catch (err) {
-    console.log(err)
-  }
-})
+  app.put("/api/workouts/:id", ({ params, body }, res) => {
+      console.log(body)
+    db.Workout.findByIdAndUpdate(
+        {_id: params.id}, 
+        {$push:{exercises: body}}, 
+        { new: true, runValidators: true }
+    ).then(data => res.json(data))
+    .catch(err => console.log(err))
+  });
 
-app.put("/api/workout:id", ({body, params}), res => {
-  const workoutID = params.id;
-  let savedExercises= [];
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body).then((newWorkout) => {
+      res.json(newWorkout);
+    });
+  });
 
-  db.workout.find({_id:workoutID}).then(dbworkout => {
-    savedExercises=dbworkout[0].exercises;
-    res.json(dbworkout[0].exercises);
-    let allExercises = [...savedExercises, body]
-    console.log(allExercises)
-    updateWorkout(allExercises)
-  })
-  .catch(err => {
-    res.json(err);
-});
-
-function updateWorkout(exercises){
-db.Workout.findByIdAndUpdate(workoutId, {exercises: exercises}, function(err, doc){
-if(err){
-    console.log(err)
-}
-
-})
-}
-
-})
-
-app.get("/api/workouts/range", (req, res) => {
-db.Workout.find({})
-.then(workout => {
-res.json(workout);
-})
-.catch(err => {
-res.json(err);
-});
-});
+  app.get("/api/workouts/range", (req, res) => {
+      db.Workout.aggregate().sort({
+          _id: -1
+      }).limit(7).then((pastWorkout) => {
+        res.json(pastWorkout);
+    })
+    })
+};
--- routes/html.js
+++ routes/html.js
@@ -1,18 +1,18 @@
 var path = require("path");
-const db = require("../models")
 
 
-module.exports = function(app){
 
-  app.get("./", function (req, res) {
+module.exports = (app) => {
+
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  app.get("./exercise", function (req, res) {
+  app.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
 
-  app.get("./stats", function (req, res) {
+  app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 }
